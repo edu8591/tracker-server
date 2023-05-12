d12aa79df546d2d877e3ad83582c6bd55632e639
Eduardo Serrano <eduardoserrano8591@gmail.com>
2023-05-12 15:32:30 -0500

    add functionality to salt, hash and compare a password

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require("mongoose");
-
+const bcrypt = require("bcrypt");
 const userSchema = new Schema({
   email: {
     type: String,
@@ -12,5 +12,41 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      next(err);
+    }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.compatePasswords = function (candidatePassword) {
+  const user = this;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+
+      if (!isMatch) {
+        return reject(false);
+      }
+
+      resolve(true);
+    });
+  });
+};
 
 model("User", userSchema);
